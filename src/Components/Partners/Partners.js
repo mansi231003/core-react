6d51cd59cbd9c401fd55d439cf244bc317c5e72a
mansi231003 <mansidevgan000@gmail.com>
2025-12-06 12:42:50 +0530

    initial commit

---
+++ src/Components/Partners/Partners.js
@@ -0,0 +1,19 @@
+import "./Partners.css"
+export default function Partners(){
+    return(
+        <>
+        <div className="outer-container">
+        <div className="partners-container">
+            <div className="worked-with">Worked with</div>
+          <div className="work-icons">
+            <div className="icons"><img className="work-images" src="clickup.png"/></div>
+            <div className="icons"><img className="work-images" src="dropbox.png"/></div>
+            <div className="icons"><img className="work-images" src="paychex.png"/></div>
+            <div className="icons"><img className="work-images" src="/Elastic logo 1.png"/></div>
+            <div className="icons"><img className="work-images" src="stripe.png"/></div>
+          </div>
+        </div>
+        </div>
+        </>
+    )
+}
